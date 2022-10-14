7d399fc506828caea81d8abdff547c99753136ce
Andrew Shaw <shandyshawshank@gmail.com>
2022-10-14 16:29:20 -0600

    spring boot

---
+++ DogParks/src/main/java/com/skilldistillery/dogparks/DogParksApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.dogparks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DogParksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DogParksApplication.class, args);
+	}
+
+}
---
+++ DogParks/src/test/java/com/skilldistillery/dogparks/DogParksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.dogparks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DogParksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
